a05a1b538ad07ae2145c9c9ce4621906986891fa
twer <twer@MobileLab-iMac.local>
2012-06-07 12:31:02 +0800

    Initial Commit

---
+++ HelloSample/TWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWAppDelegate.h
+//  HelloSample
+//
+//  Created by twer on 6/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
